d16407c0c980973f4e16925aee639510fe595e61
James Lannon <jamesmlannon1990@live.com>
2017-05-05 16:55:34 -0700

    I can either print my array or print my pingpong function on only the number entered, can't combine them as of yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
 // back end logic
 
 
-function pingPong(number) {
+var pingPong = function(number) {
+  var numberArray = [];
+
+// this is pushing the userinput into the blank numberArray
   for (var index = 1; index <= number; index += 1) {
-    if ((index % 3 === 0) && (index % 5 !== 0) && (index % 15 !== 0)) {
+    numberArray.push(index);
+  }
+  console.log(numberArray);
+  return numberArray
+
+// this inserts ping, pong or ping-pong in place of numbers divisible by 3, 5 or 15
+
+  var numberManipulation = numberArray.map(function(number) {
+    if ((number % 3 === 0) && (number % 5 !== 0) && (number % 15 !== 0)) {
       return "ping";
-    } else if ((index % 5 === 0) && (index % 15 !== 0) && (index % 3 !== 0)){
+    } else if ((number % 5 === 0) && (number % 15 !== 0) && (number % 3 !== 0)){
       return "pong";
-    } else if ((index % 15 === 0) && (index % 5 === 0) && (index % 3 === 0)){
+    } else if ((number % 15 === 0) && (number % 5 === 0) && (number % 3 === 0)){
       return "ping-pong";
     } else {
-      return index
+      return number
     }
-  }
+  });
+
+  numberManipulation(number);
+  return numberManipulation
+  console.log(numberManipulation);
 }
 
-
-// var replacedNumbers = function(number) {
-//
-// }
-
-// numberArray.forEach(function(number) {
-//   replacedNumbers();
-//   return numberArray;
-//   console.log(numberArray);
-// });
 
 
 // front end logic
@@ -33,10 +38,10 @@ $(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#user-number").val());
-    var result = pingPong();
+    var result = pingPong(number);
 
-    $("ul").text(result);
-    // alert(newNumbers);
+    $("#result").text(result);
+
 
   });
 });
