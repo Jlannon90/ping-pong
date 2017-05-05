87ac0027d6ba0b50efce53dad30cc14ad33231b3
James Lannon <jamesmlannon1990@live.com>
2017-05-05 09:30:26 -0700

    adjust if statements in replacedNumbers function so it returns ping, pong and ping-pong exclusively for numbers divisible by 3, 5 and 15, with 15 taking precedence.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
 
 
 var replacedNumbers = function(number) {
-  if (number % 3 === 0) {
+  if ((number % 3 === 0) && (number % 5 !== 0) && (number % 15 !== 0)) {
     return "ping";
-  } else if (number % 5 === 0) {
+  } else if ((number % 5 === 0) && (number % 15 !== 0) && (number % 3 !== 0)){
     return "pong";
-  } else if (number % 15 === 0) {
+  } else if ((number % 15 === 0) && (number % 5 === 0) && (number % 3 === 0)){
     return "ping-pong";
+  } else {
+    return number
   }
 };
 
