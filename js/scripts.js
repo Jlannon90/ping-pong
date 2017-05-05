3ec24f7929b7512d57ebd11036820cc2881fd394
James Lannon <jamesmlannon1990@live.com>
2017-05-05 09:19:26 -0700

    add variable number to collect user input, add function to replace nubers divisible by 3,5 and 15 to return with words, add variable calling on that function with the argument of the users input, add function to display result

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// back end logic
+
+
+var replacedNumbers = function(number) {
+  if (number % 3 === 0) {
+    return "ping";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else if (number % 15 === 0) {
+    return "ping-pong";
+  }
+};
+
+
+
+
+
+// front end logic
+
+$(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#user-number").val());
+    var result = replacedNumbers(number);
+
+    $("#result").text(result);
+
+  });
+});
