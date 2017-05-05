2321cd78eceb7a8b9409a7ddb6bd700a6fd20db0
James Lannon <jamesmlannon1990@live.com>
2017-05-05 15:47:40 -0700

    trying to combine everything into just one function, no luck so far

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 // back end logic
 
 
-var numberArray = [];
-
-var countToNumber = function(number) {
-
+function pingPong(number) {
   for (var index = 1; index <= number; index += 1) {
-    numberArray.push(index);
-  }
-  return numberArray;
-}
-
-
-var replacedNumbers = function(number) {
-  if ((number % 3 === 0) && (number % 5 !== 0) && (number % 15 !== 0)) {
-    return "ping";
-  } else if ((number % 5 === 0) && (number % 15 !== 0) && (number % 3 !== 0)){
-    return "pong";
-  } else if ((number % 15 === 0) && (number % 5 === 0) && (number % 3 === 0)){
-    return "ping-pong";
-  } else {
-    return number
+    if ((index % 3 === 0) && (index % 5 !== 0) && (index % 15 !== 0)) {
+      return "ping";
+    } else if ((index % 5 === 0) && (index % 15 !== 0) && (index % 3 !== 0)){
+      return "pong";
+    } else if ((index % 15 === 0) && (index % 5 === 0) && (index % 3 === 0)){
+      return "ping-pong";
+    } else {
+      return index
+    }
   }
 }
 
+
+// var replacedNumbers = function(number) {
+//
+// }
+
+// numberArray.forEach(function(number) {
+//   replacedNumbers();
+//   return numberArray;
+//   console.log(numberArray);
+// });
 
 
 // front end logic
@@ -32,12 +33,10 @@ $(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#user-number").val());
-    var result = replacedNumbers(number);
-    var test = countToNumber(number);
+    var result = pingPong();
 
-    $("#result").text(result);
-    alert(numberArray);
-
+    $("ul").text(result);
+    // alert(newNumbers);
 
   });
 });
